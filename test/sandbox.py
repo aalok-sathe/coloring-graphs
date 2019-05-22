c5b2dcf507461928f1882e6321883621e3d9d134
as9kc <aalok.sathe@richmond.edu>
2019-05-22 11:59:44 -0400

    more playing around to debug; in sandbox

--- test/sandbox.py
+++ test/sandbox.py
@@ -8,8 +8,26 @@ import sys
 sys.path.append(str((Path(__file__).parent.resolve() / '..').resolve()))
 from lib import *
 
+
+test_file = 'input/g2.in' # bipartite_test_graph0.in
+
+
 if __name__ == '__main__':
     rel = Path(__file__).parent
     graph = Graph.BaseGraph()
-    graph.load_txt(rel / 'bipartite_test_graph0.in')
+    graph.load_txt(rel / test_file)
     colgraph = graph.build_coloring_graph(3)
+
+    print(graph)
+    # print(colgraph)
+
+    for v in colgraph.get_vertices():
+        print(repr(v))
+        print('coloring repr: %s' % mathtools.convert_base(v[NAME], v[COLORS]))
+        print('possible neighbors v: %d' % len([*v.get_possible_neighbors(len(colgraph))]))
+        print('actual neighbors v: %d' % len([*colgraph.get_neighbors(v)]))
+        # for nbr in colgraph.get_neighbors(v):
+        #     print(repr(nbr))
+        #     print('nbr coloring: %s' % mathtools.convert_base(nbr[NAME],
+        #                                                       nbr[COLORS]))
+        print()
