69b5742ac558ecd7e6aa69f1793eb43bac3eca9c
as9kc <aalok.sathe@richmond.edu>
2019-05-21 21:33:56 -0400

    more impls; start implementing coloringvertex

--- lib/Vertex.py
+++ lib/Vertex.py
@@ -5,26 +5,22 @@ from pathlib import Path
 from collections import defaultdict
 import typing
 
-
-def vectorize(funct: typing.Callable) -> typing.Callable:
-    '''
-    makes a function take a list as an argument and returns a list
-    after calling the function on each element of the list
-    '''
-    def vectorized(*args):
-        return [funct(arg) for arg in arglist]
-
-    return vectorized
-
+# vertex attributes
+ID = 'id'
+COLOR = 'color'
+NAME = 'name'
 
 class Vertex:#(gt.Vertex):
     attrs = None
     neighbors = None
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, i=None):
         # super().__init__(*args)
         self.attrs = defaultdict(None)
         self.neighbors = set()
+
+        self[ID] = i
+
 
     def __getitem__(self, key):
         '''
@@ -34,6 +30,7 @@ class Vertex:#(gt.Vertex):
         '''
         return self.attrs[key]
 
+
     def __setitem__(self, key, value):
         '''
         overridden method to support setting attributes
@@ -42,9 +39,37 @@ class Vertex:#(gt.Vertex):
         self.attrs[key] = value
 
 
-    def add_neighbors(self, *vertices: typing.List[__name__]):
+    def add_neighbors(self, *vertices: typing.Container[__name__]):
         '''
         adds vertices to self's neighboring vertices
         '''
         for v in vertices:
             self.neighbors.add(v)
+
+
+    def get_neighbors(self) -> typing.Set[__name__]:
+        '''
+        returns neighbors of self
+        '''
+        return self.neighbors
+
+
+
+class ColoringVertex(Vertex):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+        def add_neighbors(self, *vertices: typing.Container[__name__]):
+            '''
+            adds vertices to self's neighboring vertices
+            '''
+            raise NotImplementedError('no support to add neighbors to a '
+                                      'ColoringVertex. please read the docs')
+
+
+        def get_possible_neighbors(self, k: int):
+            '''
+
+            '''
