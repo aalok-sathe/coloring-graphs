195a6d5da55179b9e7f1d376097aeb65ef6ddc1f
as9kc <aalok.sathe@richmond.edu>
2019-05-22 16:13:49 -0400

    less debug output

--- test/sandbox.py
+++ test/sandbox.py
@@ -15,7 +15,8 @@ from lib import *
 try:
     test_file = Path(*Path(sys.argv[1]).parts[1:])
 except IndexError:
-    test_file = 'input/g1.in' # bipartite_test_graph0.in
+    # test_file = 'input/g1.in'
+    test_file = 'input/bipartite_test_graph0.in'
 
 
 if __name__ == '__main__':
@@ -24,22 +25,8 @@ if __name__ == '__main__':
     graph.load_txt(rel / test_file)
     colgraph = graph.build_coloring_graph(3)
 
-    print(graph)
-    # print(colgraph)
-
-    # for v in colgraph.get_vertices():
-    #     print(repr(v))
-    #     print('coloring repr: %s' % mathtools.convert_base(v[NAME], v[COLORS]))
-    #     print('possible neighbors v: %d' % len([*v.get_possible_neighbors(len(colgraph))]))
-    #     print('actual neighbors v: %d' % len([*colgraph.get_neighbors(v)]))
-    #     # for nbr in colgraph.get_neighbors(v):
-    #     #     print(repr(nbr))
-    #     #     print('nbr coloring: %s' % mathtools.convert_base(nbr[NAME],
-    #     #                                                       nbr[COLORS]))
-    #     print()
-
     draw_fn = nx.draw
-    kwargs = dict(with_labels=1, node_size=512)
+    kwargs = dict(with_labels=1, node_size=2048, font_size=10)
 
     plt.subplot(1, 2, 1)
     plt.xlabel(graph.__class__.__name__)
