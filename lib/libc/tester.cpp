815b55088643fda96168211755d13e0375852cad
as9kc <aalok.sathe@richmond.edu>
2019-05-23 17:02:01 -0400

    why wont it compile on Mac?

---
+++ lib/libc/tester.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <cstdlib>
+#include "Graph.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // cout << "before init!" << endl;
+    //
+    // Graph g;
+    //
+    // cout << "after init!" << endl;
+
+    return 0;
+}
